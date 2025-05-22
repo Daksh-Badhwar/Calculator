ba6ac592eb1dd51e17988a8380bf5ee9a85b9faa
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-22 18:01:11 +0530

    UseState and functionality or behaviour with data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import styles from  './App.module.css';
 import Display from './components/Display';
 import ButtonsContainer from "./components/ButtonsContainer"
+
  
 //repalce id by classname //div means container
 function App() {
+
+//we are not only concerned with display but button too so mention state in App
+const[calVal,setCalVal]= useState("")   //initial display empty
+//button text can be replaced by event.target.data
+const onButtonClick=(buttonText)=>{
+  if (buttonText==='C') setCalVal("");
+  else if (buttonText==='=')
+  {
+    const result =eval(calVal);
+     setCalVal(result);   
+  }
+  else
+  {
+     const newDisplayValue= calVal + buttonText;
+     setCalVal(newDisplayValue);
+  }
+
+}
+
+
   return (
     <div className ={styles.calculator}> 
-     <Display></Display>
-     <ButtonsContainer></ButtonsContainer>
+     <Display displayValue={calVal}></Display>
+     <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
      
 
 
--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import styles from  "./ButtonsContainer.module.css";
 
 
-const ButtonsContainer=()=>{
+const ButtonsContainer=({onButtonClick})=>{
 const buttonNames =['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.'];
 
 
 return (
     <div className={styles.buttonsContainer}>
-    {buttonNames.map((button)=><button className={styles.button}>{button}</button>)};
+    {buttonNames.map((button)=><button className={styles.button} onClick={()=>{onButtonClick(button)}}>{button}</button>)};
       
      </div>
 );
